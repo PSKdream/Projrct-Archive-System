408827f3a5ca92e3eb2add9b4b7610720dd5f857
mrfoviwe <69419853+mrfoviwe@users.noreply.github.com>
2021-10-26 14:28:04 +0700

    pull login kai dream

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { AdminComponent } from './admin/admin.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'admin', component: AdminComponent },
   { path: '', component: HomeComponent },
   { path: '**', redirectTo: 'home', pathMatch: 'full' },
+  
 ];
 
 @NgModule({
