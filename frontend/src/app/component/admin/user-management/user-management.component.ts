56095b10c0fc646118e4260568bbe30230293e01
mrfoviwe <69419853+mrfoviwe@users.noreply.github.com>
2021-10-28 02:00:15 +0700

    create btn-insert

--- frontend/src/app/component/admin/user-management/user-management.component.ts
+++ frontend/src/app/component/admin/user-management/user-management.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,NgZone } from '@angular/core';
+import { LoginService } from '../../../service/login/login.service';
+import { Router } from "@angular/router";
+import {
+  FormControl,
+  FormGroup,
+  FormBuilder,
+  Validator,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-user-management',
   templateUrl: './user-management.component.html',
-  styleUrls: ['./user-management.component.scss']
+  styleUrls: ['./user-management.component.scss'],
 })
 export class UserManagementComponent implements OnInit {
+  bioSection = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+    firstname: new FormControl(''),
+    lastname: new FormControl(''),
+    role: new FormControl(''),
+  });
 
-  constructor() { }
+  constructor(private _LoginService: LoginService,private _NgZone: NgZone, private _Router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  submitUser() {
+    console.log(this.bioSection.value);
+    if (this.bioSection.value.password.length <= 8) {
+      console.log('......');
+      return
+    }
+    if (
+      this.bioSection.value.username === '' ||
+      this.bioSection.value.password === '' ||
+      this.bioSection.value.firstname === '' ||
+      this.bioSection.value.lastname === '' ||
+      this.bioSection.value.role === ''
+    ) {
+      console.log('Wrong');
+      return
+
+    }
+    this._LoginService.addUser(this.bioSection.value).subscribe(()=>{
+      this._NgZone.run(()=> this._Router.navigateByUrl("/admin/user-management"))
+    },(error)=>{
+      console.log(error);
+    })
+
   }
-
 }
