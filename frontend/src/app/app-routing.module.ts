5277fa5d2e09488a54d6e992565a75b1f933963a
PSK-Dream <49468390+PSKdream@users.noreply.github.com>
2021-10-27 09:20:54 +0700

    relevel folder

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminComponent } from './admin/admin.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './component/admin/admin.component';
+import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,9 +4,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AdminComponent } from './admin/admin.component';
-import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './component/home/home.component';
+import { AdminComponent } from './component/admin/admin.component';
+import { LoginComponent } from './component/login/login.component';
 
 @NgModule({
   declarations: [
