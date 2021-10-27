d12ae06766749fe4ded6739ecde98f7c0966ac5a
mrfoviwe <69419853+mrfoviwe@users.noreply.github.com>
2021-10-27 18:53:57 +0700

    create param

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './component/home/home.component';
 import { AdminComponent } from './component/admin/admin.component';
 import { LoginComponent } from './component/login/login.component';
+import { UserManagementComponent } from './component/admin/user-management/user-management.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AdminComponent,
-    LoginComponent
+    LoginComponent,
+    UserManagementComponent
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/component/admin/admin.component.ts
+++ frontend/src/app/component/admin/admin.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';  
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  styleUrls: ['./admin.component.scss'],
 })
 export class AdminComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router  
+  ) {}
 
   ngOnInit(): void {
+    let params = this.route.snapshot.paramMap;
+    // if (params.has('id')) {
+    //   // this.highlightId = params.get('id');
+    // }
+    console.log(params);
   }
-
 }
-
-
