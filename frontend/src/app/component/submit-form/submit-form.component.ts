60669dc9d37a8c2021c2b6cb09b9431450cf866c
NorrapaTT <69419738+norgammerzz@users.noreply.github.com>
2021-10-29 17:41:20 +0700

    add-controlname

--- frontend/src/app/component/submit-form/submit-form.component.ts
+++ frontend/src/app/component/submit-form/submit-form.component.ts
@@ -8,6 +8,18 @@ import { FormControl, FormGroup, FormBuilder} from "@angular/forms";
 })
 export class SubmitFormComponent implements OnInit {
   arr = [0] 
+  submit_form = new FormGroup({
+    course: new FormControl(''),
+    graduation_year : new FormControl(''),
+    project_type : new FormControl(''),
+    nameTH : new FormControl(''),
+    nameEng : new FormControl(''),
+    developID : new FormControl(''),
+    developName : new FormControl(['']),
+    abstract : new FormControl(''),
+    sorec_code : new FormControl(''),
+    advisor_name: new FormControl([''])
+  });
   constructor() { }
   ngOnInit(): void { 
   }
@@ -15,6 +27,11 @@ export class SubmitFormComponent implements OnInit {
     console.log(number);
     if(number === 1){
       this.arr.push(this.arr[this.arr.length-1]+1)
+      this.submit_form.value.developName.push('');
+      console.log(this.submit_form.value.developName);
+      // this.submit_form.patchValue({
+      //   developName: this.submit_form.developNa
+      // })
     }
     else if(number === -1 && this.arr.length>1){
       this.arr.pop()
