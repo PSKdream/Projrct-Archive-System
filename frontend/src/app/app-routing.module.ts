0ba5c566c50ed968c88b1f336cd689026255b508
PSK-Dream <49468390+PSKdream@users.noreply.github.com>
2021-10-30 12:38:33 +0700

    create api project

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { AdminComponent } from './component/admin/admin.component';
 import { HomeComponent } from './component/home/home.component';
 import { LoginComponent } from './component/login/login.component';
 import { TestComponent } from './component/test/test.component';
-import { ProjectListComponent } from './component/project-list/project-list.component';
+import { ProjectListComponent } from './component/admin/project-list/project-list.component';
 import { SubmitFormComponent } from './component/submit-form/submit-form.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
